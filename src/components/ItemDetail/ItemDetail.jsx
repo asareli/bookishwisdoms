4154a9155cb40d8488714b0bd104dbaeb18c55ab
asareli <aracelimj317@gmail.com>
2024-01-24 23:15:57 -0300

    mapsandpromises

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -6,7 +6,7 @@ const ItemDetail = ({id, name, price, img}) => {
         <h2>Name: {name}</h2>
         <h3>Price: {price} </h3>
         <p>ID: {id} </p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias sapiente ad, quibusdam voluptatem eaque consectetur voluptatibus voluptatum necessitatibus, enim, perferendis provident. Unde veritatis nam nesciunt illo dignissimos non facilis itaque?</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi inventore magni cum accusantium eius. Soluta quas ratione veniam cumque odit odio impedit delectus sint! Et minus placeat eligendi explicabo iure.</p>
         <img src="{img}" alt="{name}" />
     </div>
   )
